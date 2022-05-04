6bc2952122dfa81c3eec0ca22c1eceb9166f8005
Sanket Naik <sanketnaik99@gmail.com>
2022-05-03 22:15:52 -0700

    Redux boilerplate code.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { configureStore } from "@reduxjs/toolkit";
+import reducer from "../redux/modules/index";
+import { Provider } from "react-redux";
+
+const store = configureStore({ reducer });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ redux/modules/counter.ts
@@ -0,0 +1,38 @@
+const INCREMENT = "next-notes/counter/INCREMENT";
+const DECREMENT = "next-notes/counter/DECREMENT";
+
+interface Action {
+  type: string;
+}
+
+interface CounterState {
+  count: number;
+}
+
+const initialState: CounterState = {
+  count: 0,
+};
+
+const reducer = (
+  state: CounterState = initialState,
+  action: Action = { type: "" }
+) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, count: state.count + 1 };
+    case DECREMENT:
+      return { ...state, count: state.count + 1 };
+    default:
+      return state;
+  }
+};
+
+export const increment = () => {
+  return { type: INCREMENT };
+};
+
+export const decrement = () => {
+  return { type: DECREMENT };
+};
+
+export default reducer;
