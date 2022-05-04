0066b5f611db4e096a15e2a966d4bd967529fed0
Sanket Naik <sanketnaik99@gmail.com>
2022-05-04 12:44:47 -0700

    Removed old test

--- cypress/integration/demo.spec.ts
+++
@@ -1,7 +0,0 @@
-it("Testing Cypress Integration Test", () => {
-  cy.visit("http://localhost:3000");
-
-  cy.get("h1").contains("Welcome to Next.js!");
-
-  cy.get("p").contains("Get started by editing");
-});
